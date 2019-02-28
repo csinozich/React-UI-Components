43ad83bc0e4c5fffd69450bf63098e9ede2764bb
Claire Sinozich <claire.sinozich@gmail.com>
2019-02-27 19:45:26 -0700

    Header setup

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+
+import HeaderTitle from './components/HeaderComponents/HeaderTitle';
+import ImageThumbnail from './components/HeaderComponents/ImageThumbnail';
+import HeaderContent from './components/HeaderComponents/HeaderContent';
+
+
+const HeaderContainer = () => {
+  return (
+    <div>
+      <ImageThumbnail />
+      <HeaderTitle />
+      <HeaderContent />
+    </div>
+  );
+};
+
+export default HeaderContainer;
