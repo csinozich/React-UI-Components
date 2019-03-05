67ae3607d9d7e80d57e01d47cccf4ebdf9cf0362
Claire Sinozich <claire.sinozich@gmail.com>
2019-03-05 11:13:09 -0700

    Calculator MVP-ish

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,23 +1,42 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
 
-const App = () => {
+
+const App = props => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className='calculator-style'>
+      <CalculatorDisplay />
+      <container>
+        <ActionButton className = 'action-button' text='clear' />
+        <NumberButton className='red-button' text='/'/>
+      </container>
+      <container>
+        <NumberButton className = 'white-button' text='7' />
+        <NumberButton className = 'white-button' text='8' />
+        <NumberButton className = 'white-button' text='9' />
+        <NumberButton className = 'red-button' text='X' />
+      </container>
+      <container>
+        <NumberButton className = 'white-button' text='4' />
+        <NumberButton className = 'white-button' text='5' />
+        <NumberButton className = 'white-button' text='6' />
+        <NumberButton className = 'red-button' text='-' />
+      </container>
+      <container>
+        <NumberButton className = 'white-button' text='1' />
+        <NumberButton className = 'white-button' text='2' />
+        <NumberButton className = 'white-button' text='3' />
+        <NumberButton className = 'red-button' text='+' />
+      </container>
+      <container>
+        <ActionButton className = 'action-button' text='0' />
+        <NumberButton className = 'red-button' text='=' />
+      </container>
     </div>
-  );
-};
+  )
+}
 
 export default App;
